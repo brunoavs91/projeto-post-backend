ba4849138703287103f3c0cc554484d77501d828
brunoav91@gmail.com <brunoav91@gmail.com>
2020-07-01 13:32:16 -0300

    subindo projeto backend

---
+++ projetoPost/src/test/java/com/bruno/post/ProjetoPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
