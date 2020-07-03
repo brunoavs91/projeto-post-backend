c42f7365b2101c7a6814727512b6fef4791ba336
brunoav91@gmail.com <brunoav91@gmail.com>
2020-07-03 00:15:13 -0300

    continue

--- projetoPost/src/main/java/com/bruno/post/service/impl/PostServiceImpl.java
+++ projetoPost/src/main/java/com/bruno/post/service/impl/PostServiceImpl.java
@@ -1,6 +1,9 @@
 package com.bruno.post.service.impl;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bruno.post.domain.Post;
 import com.bruno.post.dto.PostDTO;
@@ -10,8 +13,23 @@ import com.bruno.post.service.PostService;
 public class PostServiceImpl implements PostService{
 
 	
-	public Post convertFromPost (PostDTO postDTO) {
+//	public Post convertFromPost (PostDTO postDTO) {
+//		Post post = new Post();
+//		post.setId(postDTO.getId());
+//		post.setComentario(postDTO.getComentario());
+//		post.setImagem(postDTO.getImagem());
+//		
+//		return null;
+//	}
+	
+	public void saveImage (MultipartFile multipartFile) {
+		try {
+			byte[] bytes = multipartFile.getBytes();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 		
-		return null;
 	}
 }
--- projetoPost/src/main/java/com/bruno/post/service/impl/UsuarioServiceImpl.java
+++ projetoPost/src/main/java/com/bruno/post/service/impl/UsuarioServiceImpl.java
@@ -1,12 +1,34 @@
 package com.bruno.post.service.impl;
 
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import org.hibernate.hql.internal.CollectionSubqueryFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import com.bruno.post.domain.Usuario;
+import com.bruno.post.dto.UsuarioDTO;
 import com.bruno.post.service.UsuarioService;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
 
+	public void insert(UsuarioDTO usuarioDTO) {
+		
+		
+	}
 	
+//	public Usuario convertFromUsuario (UsuarioDTO usuarioDTO) {
+//		
+//		Usuario usuario = new Usuario();
+//		usuario.setId(usuarioDTO.getId());
+//		usuario.setNome(usuarioDTO.getNome());
+//		usuario.setEmail(usuarioDTO.getEmail());
+//		usuario.setPosts(new ArrayList<>());
+//		
+//		if(CollectionU)
+//		
+//	}
 	
 }
